b4e7bb491224b4f94f17223e9c061ed7ca8febf2
efrencodes <efrencodes@gmail.com>
2022-05-15 02:53:10 -0500

    FEAT Update description_place widget

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
+  String namePlace;
+  int star;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.star, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
+    final star_half = Container(
+        margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+        child: const Icon(Icons.star_half, color: Color(0xFFf2C611)));
+
+    final star_border = Container(
+        margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+        child: const Icon(Icons.star_border, color: Color(0xFFf2C611)));
+
     final star = Container(
         margin: const EdgeInsets.only(top: 323.0, right: 3.0),
         child: const Icon(Icons.star, color: Color(0xFFf2C611)));
 
-    final title_stars = Row(children: <Widget>[
+    final title_stars = Row(children: [
       Container(
           margin: const EdgeInsets.only(top: 320.00, left: 20.00, right: 20.0),
-          child: const Text('Duwili Ella',
+          child: new Text(namePlace,
               style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
               textAlign: TextAlign.left)),
       Row(
-        children: [star, star, star, star, star],
+        children: [star, star, star, star_half, star_border],
       )
     ]);
 
-    return title_stars;
+    final description = Container(
+        margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+        child: new Text(descriptionPlace,
+            style: const TextStyle(
+                fontSize: 14.0,
+                letterSpacing: 0.1,
+                fontWeight: FontWeight.normal,
+                color: Color(0xFF56575a))));
+
+    final description_place = Column(
+      children: [title_stars, description, description],
+    );
+
+    return description_place;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'description_place.dart';
 
 void main() => runApp(const MyApp());
 
+String descriptionDummy =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           appBar: AppBar(title: const Text('Hola mundo Feliz')),
-          body: DescriptionPlace()),
+          body: DescriptionPlace('Bahamas', 2, descriptionDummy)),
     );
   }
 }
