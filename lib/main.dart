095e889d9db58e2e1557b8953caedcf70a220c12
efrencodes <efrencodes@gmail.com>
2022-05-15 02:12:15 -0500

    FEAT Project initial and description_place

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+          appBar: AppBar(title: const Text('Hola mundo Feliz')),
+          body: DescriptionPlace()),
+    );
+  }
+}
